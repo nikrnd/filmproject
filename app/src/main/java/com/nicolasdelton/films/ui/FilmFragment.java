ccb2bc5c5c4c74af24f0c0a8f6200b51c7dfd18b
nikrnd <delton.nicolas@gmail.com>
2021-04-25 00:15:03 +0200

    update

--- app/src/main/java/com/nicolasdelton/films/ui/FilmFragment.java
+++ app/src/main/java/com/nicolasdelton/films/ui/FilmFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.nicolasdelton.films.R;
 import com.nicolasdelton.films.film.Film;
 import com.nicolasdelton.films.film.Noleggio;
@@ -37,18 +44,69 @@ public class FilmFragment extends Fragment {
         titolo = root.findViewById(R.id.titolo);
         trama = root.findViewById(R.id.trama);
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                System.out.println("##########" + codice.getText() + "##");
+//                System.out.println("##########" + titolo.getText() + "##");
+//                System.out.println("##########" + trama.getText() + "##");
+
+                if (codice.getText().toString().equals("") ||
+                titolo.getText().toString().equals("") ||
+                trama.getText().toString().equals("")) {
+                    System.out.println("###");
+                    Toast.makeText(requireContext(), "Inserisci tutti i campi", Toast.LENGTH_SHORT).show();
+                } else {
+                    inserisciFilm();
+                    Toast.makeText(requireContext(), "Film inserito", Toast.LENGTH_SHORT).show();
+                    codice.setText("");
+                    titolo.setText("");
+                    trama.setText("");
+                }
+            }
+        });
+
         return root;
     }
 
     private void inserisciFilm(){
         films.add(new Film(Integer.parseInt(codice.getText().toString()), titolo.getText().toString(), trama.getText().toString()));
+        databaseAdd();
     }
 
-    private void inserisciNoleggio(Film film){
+    Integer value = 1;
 
-    }
+    private void databaseAdd(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Film/value");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                value = ((dataSnapshot.getValue(Integer.class)) + 1);
+            }
 
-    private void databaseAdd(Film film){
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
 
+        DatabaseReference ccDB = database.getReference("Film/value");
+        ccDB.setValue(value);
+
+        DatabaseReference codiceDB = database.getReference("Film/" + value + "/codice");
+        codiceDB.setValue(codice.getText().toString());
+
+        DatabaseReference titoloDB = database.getReference("Film/" + value + "/titolo");
+        titoloDB.setValue(titolo.getText().toString());
+
+        DatabaseReference tramaDB = database.getReference("Film/" + value + "/trama");
+        tramaDB.setValue(trama.getText().toString());
+
+
+       // Film film = new Film(Integer.parseInt(codice.getText().toString()), titolo.getText().toString(), trama.getText().toString());
     }
 }
--- app/src/main/java/com/nicolasdelton/films/ui/NoleggioFragment.java
+++ app/src/main/java/com/nicolasdelton/films/ui/NoleggioFragment.java
@@ -1,9 +1,12 @@
 package com.nicolasdelton.films.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,17 +15,121 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nicolasdelton.films.R;
+import com.nicolasdelton.films.film.Film;
+
+import java.util.ArrayList;
 
 public class NoleggioFragment extends Fragment {
+
+    private ArrayList<Film> films;
+    private Spinner chooseSpinner;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_noleggio, container, false);
 
+        films = new ArrayList<>();
+
+        syncDB();
+
+        for (int i = 0; i < films.size(); i++) {
+            System.out.println("############");
+            System.out.println(films.get(i).getCodice());
+            System.out.println(films.get(i).getTitolo());
+            System.out.println(films.get(i).getTrama());
+        }
+
+//        chooseSpinner = root.findViewById(R.id.chooseFilm);
+//        ArrayAdapter<Film> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_dropdown_item, films);
+//        chooseSpinner.setAdapter(adapter);
+
 
 
         return root;
     }
+
+    private Integer value = 1, codice;
+    private String titolo, trama;
+
+    private void syncDB() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Film/value");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                value = ((dataSnapshot.getValue(Integer.class)) + 1);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
+        for (int i = 1; i <= value; i++) {
+            DatabaseReference refCodice = database.getReference("Film/" + i + "/codice");
+            // Read from the database
+            refCodice.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                     codice = dataSnapshot.getValue(Integer.class);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                }
+            });
+
+            DatabaseReference refTitolo = database.getReference("Film/" + i + "/titolo");
+            // Read from the database
+            refTitolo.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    titolo = dataSnapshot.getValue(String.class);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                }
+            });
+
+            DatabaseReference refTrama = database.getReference("Film/" + i + "/trama");
+            // Read from the database
+            refTrama.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    trama = dataSnapshot.getValue(String.class);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                }
+            });
+
+            //films.add(new Film(Integer.parseInt(codice), titolo, trama));
+
+            System.out.println("####" + codice + "; " + titolo + "; " + trama);
+        }
+    }
+
+
 }
