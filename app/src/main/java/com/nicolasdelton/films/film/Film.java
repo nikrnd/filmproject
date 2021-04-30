74d0fa89ebf6e95ad503963662703a2c280bb47f
nikrnd <delton.nicolas@gmail.com>
2021-04-30 09:18:42 +0200

    JavaDoc comments

--- app/src/main/java/com/nicolasdelton/films/film/Film.java
+++ app/src/main/java/com/nicolasdelton/films/film/Film.java
@@ -1,53 +1,99 @@
 package com.nicolasdelton.films.film;
 
+/**
+ * Classe per la creazione e gestione di ogeetti di tipo "Film"
+ * @author nicolasdelton
+ */
 public class Film {
 	private int codice;
 	private String titolo;
 	private String trama;
-	
+
+	/**
+	 * Costruttore per la creazione e inizializzatone di un oggetto di tipo "Film"
+	 * @param codice Codice del film
+	 * @param titolo Titolo del film
+	 * @param trama Trama del film
+	 */
 	public Film(int codice, String titolo, String trama) {
 		this.codice = codice;
 		this.titolo = titolo;
 		this.trama = trama;
 	}
-	
+
+	/**
+	 * Costruttore di copia
+	 * @param film Film da copiare
+	 */
 	public Film(Film film) {
 		codice = film.codice;
 		titolo = film.titolo;
 		trama = film.trama;
 	}
-	
+
+	/**
+	 * Per calcolare il costo del film in base ai giorni
+	 * @param giorni Giorni di noleggio
+	 * @return Prezzo del noleggio
+	 */
 	public int calcoloCosto(int giorni) {
 		return giorni * 2;
 	}
-	
+
+	/**
+	 * Per confrontare due film
+	 * @param film Film da confrontare
+	 * @return <ul><li>[true] sono uguali</li><li>[false] sono diverse</li></ul>
+	 */
 	public boolean equals(Film film) {
 		return codice == film.codice;
 	}
 
+	/**
+	 * Restituisce il codice del film
+	 * @return Codice del film
+	 */
 	public int getCodice() {
 		return codice;
 	}
 
+	/**
+	 * Inserisce il codice del film
+	 * @param codice Codice del film
+	 */
 	public void setCodice(int codice) {
 		this.codice = codice;
 	}
 
+	/**
+	 * Restituisce il titolo del film
+	 * @return Titolo del film
+	 */
 	public String getTitolo() {
 		return titolo;
 	}
 
+	/**
+	 * Inserisce il titolo del film
+	 * @param titolo Titolo del film
+	 */
 	public void setTitolo(String titolo) {
 		this.titolo = titolo;
 	}
 
+	/**
+	 * Restituisce la trama del film
+	 * @return Trama del film
+	 */
 	public String getTrama() {
 		return trama;
 	}
 
+	/**
+	 * Inserisce la trama del film
+	 * @param trama Trama del film
+	 */
 	public void setTrama(String trama) {
 		this.trama = trama;
 	}
-	
-	
 }
--- app/src/main/java/com/nicolasdelton/films/film/Noleggio.java
+++ app/src/main/java/com/nicolasdelton/films/film/Noleggio.java
@@ -1,11 +1,23 @@
 package com.nicolasdelton.films.film;
 
+/**
+ * Classe per la creazione e gestione di oggetti di tipo "Noleggio"
+ * @author nicolasdelton
+ * @see Film
+ */
 public class Noleggio {
 	private Film film;
 	private int idCliente;
 	private int giorni;
 	private int giorniRitardo;
-	
+
+	/**
+	 * Costruttore per la creazione ed inizialiazzazione di un oggetto di tipo "Noleggio"
+	 * @param film Oggetto di tipo "Film" da noleggiare
+	 * @param idCliente Id del cliente che noleggia
+	 * @param giorni Giorni di noleggio
+	 * @param giorniRitardo Giorni di ritardo nel noleggio
+	 */
 	public Noleggio(Film film, int idCliente, int giorni, int giorniRitardo) {
 		this.film = film;
 		this.idCliente = idCliente;
@@ -13,36 +25,68 @@ public class Noleggio {
 		this.giorniRitardo = giorniRitardo;
 	}
 
+	/**
+	 * Restituisce l'id del cliente
+	 * @return Id del cliente
+	 */
 	public int getIdCliente() {
 		return idCliente;
 	}
 
+	/**
+	 * Inserisce l'id del cliente
+	 * @param idCliente Id del cliente
+	 */
 	public void setIdCliente(int idCliente) {
 		this.idCliente = idCliente;
 	}
 
+	/**
+	 * Restituisce i giorni di noleggio
+	 * @return Giorni di noleggio
+	 */
 	public int getGiorni() {
 		return giorni;
 	}
 
+	/**
+	 * Inserisce i giorni di noleggio
+	 * @param giorni Giorni di noleggio
+	 */
 	public void setGiorni(int giorni) {
 		this.giorni = giorni;
 	}
 
+	/**
+	 * Restituisce i giorni di ritardo
+	 * @return Giorni di ritardo
+	 */
 	public int getGiorniRitardo() {
 		return giorniRitardo;
 	}
 
+	/**
+	 * Inserisce i giorni di ritardo
+	 * @param giorniRitardo Giorni di ritardo
+	 */
 	public void setGiorniRitardo(int giorniRitardo) {
 		this.giorniRitardo = giorniRitardo;
 	}
 
+	/**
+	 * Restituisce il film noleggiato
+	 * @return Film noleggiato
+	 */
 	public Film getFilm() {
 		return film;
 	}
 
+	/**
+	 * Inserisce il film noelggiato
+	 * @param film Film noleggiato
+	 */
 	public void setFilm(Film film) {
 		this.film = film;
 	}
-	
+
 }
